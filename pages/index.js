ab05853d00a3cc8f755425cd54147a61ccfc7e50
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-03-04 17:10:26 +0100

    Next js app development

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
 import Heading from "@/components/Heading";
 import styles from '../styles/Home.module.scss';
 import Head from "next/head";
-const Home = () => {
+import Socials from "@/components/Socials";
+
+export const getStaticProps = async() => {
+  const response = await fetch(`${process.env.API_HOST}/socials/`);
+  const data = await response.json();
+
+  if(!data) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      socials: data
+    }
+  }
+
+} 
+
+const Home = ({socials}) => {
   return (
     <div className={styles.wrapper}>
       <Head>
         <title>Home</title>
       </Head>
       <Heading text="Hello Next"/>
+      <Socials socials={socials}/>
     </div>
   )
 }
